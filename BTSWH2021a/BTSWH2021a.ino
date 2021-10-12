5ba3ee8635f579d0e07f800616b87b2425be836d
tinyboxxx <hcxtxwd_ok@126.com>
2021-10-12 20:35:58 +0800

    代码更新、Readme更新

--- BTSWH2021a/BTSWH2021a.ino
+++ BTSWH2021a/BTSWH2021a.ino
@@ -1,78 +1,52 @@
-/*
-   This code programs a number of pins on an ESP32 as buttons on a BLE gamepad
-
-   It uses arrays to cut down on code
-
-   Uses the Bounce2 library to debounce all buttons
-
-   Uses the rose/fell states of the Bounce instance to track button states
-
-   Before using, adjust the numOfButtons, buttonPins and physicalButtons to suit your senario
-
-*/
-
 #define BOUNCE_WITH_PROMPT_DETECTION // Make button state changes available immediately
-
+//！需要安装以下三个库
 #include <ESP32Encoder.h> // https://github.com/madhephaestus/ESP32Encoder
 #include <Bounce2.h>      // https://github.com/thomasfredericks/Bounce2
 #include <BleGamepad.h>   // https://github.com/lemmingDev/ESP32-BLE-Gamepad
 
-// Create encoder objects
-ESP32Encoder encoder1;
+ESP32Encoder encoder1; // Create encoder objects
 ESP32Encoder encoder2;
-// Variables to store the current encoder count
-int32_t encoder1Count = 0;
+int32_t encoder1Count = 0; // Variables to store the current encoder count
 int32_t encoder2Count = 0;
-
 #define ENC1_LeftButton 5
 #define ENC1_RightButton 6
 #define ENC2_LeftButton 15
 #define ENC2_RightButton 16
-
 #define numOfButtons 16
 
 Bounce debouncers[numOfButtons];
-BleGamepad bleGamepad("Wireless Button Hub", "Tinyboxxx", 100); //Shows how you can customise the device name, manufacturer name and initial battery level
-
-byte buttonPins[numOfButtons] = {33, 25, 14, 39, 32, 34, 35, 36, 2, 5, 17, 18, 21, 19, 22, 23};
+BleGamepad bleGamepad("SteeringWheel Wireless Hub", "Tinyboxxx", 100); //Shows how you can customize the device name, manufacturer name and initial battery level
+byte buttonPins[numOfButtons] = {33, 25, 14, 39, 32, 34, 35, 36, 2, 5, 17, 18, 21, 19, 22, 23}; //每个按钮所在的实际IO位置
 //4,16, encoder2
 //26,27, encoder1
-byte physicalButtons[numOfButtons] = {1, 2, 3, 4, 7, 8, 9, 10, 11, 12, 13, 14, 17, 18, 19, 20};
-
+byte physicalButtons[numOfButtons] = {1, 2, 3, 4, 7, 8, 9, 10, 11, 12, 13, 14, 17, 18, 19, 20}; //虚拟出来电脑里显示的按钮顺序。5、6、15、16为旋转编码器
 int counter_for_battery_ADC = 0;
 int battery_level = 100;
 int battery_voltage_ADC = 0;
 float battery_voltage = 0;
-#define battery_voltage_ADC_pin 15
 
 void setup()
 {
-  // Serial.begin(115200);
-  // Serial.println("Starting BLE work!");
-
   ESP32Encoder::useInternalWeakPullResistors = UP; // Enable the weak pull up resistors
-  // Attach pins to encoders
-  encoder1.attachHalfQuad(26, 27);
+  encoder1.attachHalfQuad(26, 27);                 // Attach pins to encoders
   encoder2.attachHalfQuad(4, 16);
-  // Clear encoder counts
-  encoder1.clearCount();
+  encoder1.clearCount(); // Clear encoder counts
   encoder2.clearCount();
-  // Initialise encoder counts
-  encoder1Count = encoder1.getCount();
+  encoder1Count = encoder1.getCount(); // Initialise encoder counts
   encoder2Count = encoder2.getCount();
 
   for (byte currentPinIndex = 0; currentPinIndex < numOfButtons; currentPinIndex++)
   {
     pinMode(buttonPins[currentPinIndex], INPUT_PULLUP);
     debouncers[currentPinIndex] = Bounce();
-    debouncers[currentPinIndex].attach(buttonPins[currentPinIndex]); // After setting up the button, setup the Bounce instance :
+    debouncers[currentPinIndex].attach(buttonPins[currentPinIndex]); // After setting up the button, setup the Bounce instance
     debouncers[currentPinIndex].interval(5);
   }
-  bleGamepad.begin(20, 0, false, false, false, false, false, false, false, false, false, false, false, false, false);
+  bleGamepad.begin(20, 0, false, false, false, false, false, false, false, false, false, false, false, false, false); //后面的false是关闭默认开启的轴输入
   bleGamepad.setAutoReport(false);
 }
 
-void EncodersUpdate()
+void EncodersUpdate() //更新编码器
 {
   int32_t tempEncoderCount1 = encoder1.getCount();
   int32_t tempEncoderCount2 = encoder2.getCount();
@@ -143,22 +117,9 @@ void loop()
     counter_for_battery_ADC++;
     if (counter_for_battery_ADC > 500)
     {
-      battery_voltage_ADC = analogRead(15);
+      battery_voltage_ADC = analogRead(15); //15 is battery voltage ADC pin
       battery_voltage = battery_voltage_ADC * 6.6 / 4096.0;
-      battery_level = battery_voltage * 142.857 - 500;
-      // Serial.print(battery_voltage_ADC);
-      // Serial.print("\t");
-      // Serial.print(battery_voltage);
-      // Serial.print("\t");
-      // Serial.println(battery_level);
-      /*
-      x0= 4.2v y0= 100
-      x1= 3.5v y1= 0
-
-      k=142.857
-      b=-500
-      */
-
+      battery_level = battery_voltage * 142.857 - 500; //目前电量估测不准，仅供参考
       bleGamepad.setBatteryLevel(battery_level);
       counter_for_battery_ADC = 0;
     }
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # BTSWH2021a
- 无线按钮方向盘，Bluetooth Steering Wheel Hub 2021 a
+ 无线按钮方向盘，Bluetooth Steering Wheel Hub 2021
+
+# PCB
+ LINK：
+ 
+ https://oshwhub.com/tinybox/soffb_steeringwheelbutton
+
+目前原理图和PCB中仍有BUG，可通过飞线解决，但请勿盲目打板子。
